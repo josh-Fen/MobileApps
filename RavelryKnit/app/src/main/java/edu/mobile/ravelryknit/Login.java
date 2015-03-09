1543c3ff7eafb7a735d4cbaac90e808feb191df9
kspehr <spehr.4@osu.edu>
2015-03-09 19:42:51 -0400

    Revert "Revert "Changes to app to support OAuth Login""
    This reverts commit 33b3c1c7c4b05639c8d83a562c4c48da23cd25c1.

--- RavelryKnit/app/src/main/java/edu/mobile/ravelryknit/Login.java
+++ RavelryKnit/app/src/main/java/edu/mobile/ravelryknit/Login.java
@@ -3,21 +3,40 @@ package edu.mobile.ravelryknit;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.net.Uri;
 
-import io.oauth.OAuth;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.net.URLEncoder;
+
+import oauth.signpost.OAuth;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
 
 
-public class Login extends ActionBarActivity implements View.OnClickListener {
+public class Login extends Activity implements View.OnClickListener {
     private EditText username;
     private EditText password;
-    private String oauthPublicKey = "oKOmSv14kHK6kUCSIuhRaR1XBPI";
+
+    private OAuthConsumer mConsumer = new CommonsHttpOAuthConsumer("B53DF0B7F0AAB1AC65C4", "pYx+Ks/8up8wVVWgov2AsR7HSym89hWbNLclIzrJ");
+    private OAuthProvider mProvider = new CommonsHttpOAuthProvider(
+            "https://www.ravelry.com/oauth/request_token",
+            "https://www.ravelry.com/oauth/access_token",
+            "https://www.ravelry.com/oauth/authorize");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +46,38 @@ public class Login extends ActionBarActivity implements View.OnClickListener {
         password = (EditText) findViewById(R.id.password);
         View btnLogin = (Button) findViewById(R.id.submit);
         btnLogin.setOnClickListener(this);
-        //Oauth setup for login
-        final OAuth oauth = new OAuth(this);
-        oauth.initialize(oauthPublicKey);
-        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // extract the token if it exists
+        Uri uri = this.getIntent().getData();
+        String token = uri.getQueryParameter("oauth_token");
+        String verifier = uri.getQueryParameter("oauth_verifier");
+        mProvider.retrieveAccessToken(mConsumer, verifier);
+        String accessToken[] = new String[] { mConsumer.getToken(), mConsumer.getTokenSecret() };
+
+        OAuthConsumer consumer = new CommonsHttpOAuthConsumer(accessToken[0],
+                accessToken[1]);
+        HttpGet request = new HttpGet(url);
+        // sign the request
+        consumer.sign(request);
+        // send the request
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse response = httpClient.execute(request);
+    }
 
     public void onClick(View v){
         switch (v.getId()){
             case R.id.submit:
-                checkLogin();
+                String mCallbackUrl;
+                mProvider.setOAuth10a(true);
+                mCallbackUrl = ("login://cse.osu.edu" == null ? OAuth.OUT_OF_BAND :"login://cse.osu.edu");
+                String authUrl = mProvider.retrieveRequestToken(mConsumer, mCallbackUrl);
+                startActivity(new Intent("android.intent.action.VIEW", Uri.parse(authUrl)));
                 break;
         }
-    }
-
-    private void checkLogin(){
-
     }
 
     @Override
